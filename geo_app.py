182764f0f84411800679a07a6d4275a504a09167
Allesandro Yudo A <113670008+allesandroya@users.noreply.github.com>
2025-02-07 10:39:32 +0800

    Update geo_app.py

--- geo_app.py
+++ geo_app.py
@@ -25,11 +25,11 @@ if uploaded_file is not None:
     if st.checkbox("Show Data Table"):
         st.write(data)
 
-def get_color(value):
-    return [0, 255, 0, 128] if value > 0 else [255, 0, 0, 128]
+    def get_color(value):
+        return [0, 255, 0, 128] if value > 0 else [255, 0, 0, 128]
 
 # Apply the function to create a new 'color' column.
-df["color"] = df["VALUE"].apply(get_color)
+    df["color"] = df["VALUE"].apply(get_color)
 
     # Slider for radius with 0.5 km steps
     radius_km = st.slider("Select Radius (in km)", min_value=0.5, max_value=10.0, value=2.0, step=0.5)
